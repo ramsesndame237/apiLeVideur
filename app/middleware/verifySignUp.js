d07bdba36f2239feb6a279ea5a5dd00584ccec93
ramsesndame237 <ramsesndame34@gmail.com>
2022-01-05 11:55:52 +0100

    verification du roles de l'utilisateur

--- app/middleware/verifySignUp.js
+++ app/middleware/verifySignUp.js
@@ -45,3 +45,20 @@ checkDuplicateEmailOrPhoneNumber = (req, res, next) => {
         next();
     });
 }
+/**
+ * verfication de l'existance du role
+ */
+checkRolesExisted = (req, res, next) => {
+    if (req.body.roles) {
+      for (let i = 0; i < req.body.roles.length; i++) {
+        if (!ROLES.includes(req.body.roles[i])) {
+          res.status(400).send({
+            message: "Failed! Role does not exist = " + req.body.roles[i]
+          });
+          return;
+        }
+      }
+    }
+    
+    next();
+  };
